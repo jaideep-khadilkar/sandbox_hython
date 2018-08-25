aaa6cad0ebef01e18df64632d31146ff7ec19c47
jaideep-khadilkar <jaideep.khadilkar@gmail.com>
2018-08-25 06:35:31 -0700

    demo

---
+++ demo.py
@@ -0,0 +1,45 @@
+'''
+Created on 24-Aug-2018
+
+@author: user
+'''
+import sys
+sys.path.append('/opt/houdini/houdini/python2.7libs')
+import hou
+
+def create_box():
+    obj = hou.node('/obj')
+    box_geo = obj.createNode('geo','box_geo',run_init_scripts=False)
+    
+    box1 = box_geo.createNode('box','box')
+    box1.parm('scale').set(0.8)
+    
+    box2 = box_geo.createNode('box','box')
+    box2.parmTuple('size').set((0.5,0.5,0.5))
+    
+    copy = box_geo.createNode('copytopoints','copy')
+    copy.parm('transform').set(False)
+    copy.setInput(0,box2)
+    copy.setInput(1,box1)
+    
+    xform = box_geo.createNode('xform','xform')
+    xform_r = xform.parmTuple('r')
+    keyframe = hou.Keyframe()
+    keyframe.setExpression('$FF',hou.exprLanguage.Hscript)
+    xform_r[0].setKeyframe(keyframe)
+    xform_r[1].setKeyframe(keyframe)
+    xform_r[2].setKeyframe(keyframe)    
+    xform.setInput(0,copy)
+    
+    out = box_geo.createNode('null','OUT')
+    out.setInput(0,xform)
+    out.setDisplayFlag(True)
+    out.setRenderFlag(True)
+    
+    box_geo.layoutChildren()
+    
+
+def run():
+    hou.hipFile.clear(suppress_save_prompt=False)
+    create_box()
+    hou.playbar.setRealTime(True)
